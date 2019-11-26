70325e9f83d6a45f3d010d81d310307192c461a8
Shivam Bhattacharjee <shivam.bhattacharjee94@gmail.com>
2019-11-26 17:14:55 +0530

    init e2e for auth, root

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,25 +2,83 @@ import 'dotenv/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { RegisterDTO, LoginDTO } from '../src/auth/auth.dto';
+import { HttpStatus } from '@nestjs/common';
+import * as mongoose from 'mongoose';
 
-describe('AppController (e2e)', () => {
-  let app;
+const app = 'http://localhost:3000';
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+beforeAll(async () => {
+  await mongoose.connect(process.env.MONGO_URI);
+  await mongoose.connection.db.dropDatabase();
+});
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+afterAll(async done => {
+  await mongoose.disconnect(done);
+});
 
+describe('should PING root', () => {
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
+    return request(app)
       .get('/')
       .expect(200)
       .expect({
         hello: 'test object',
       });
+  });
+});
+
+describe('AUTH', () => {
+  it('should register a user', () => {
+    const user: RegisterDTO = {
+      username: 'username',
+      password: 'password',
+    };
+
+    return request(app)
+      .post('/auth/register')
+      .set('Accept', 'application/json')
+      .send(user)
+      .expect(({ body }) => {
+        expect(body.token).toBeDefined();
+        expect(body.user.username).toEqual('username');
+        expect(body.user.password).toBeUndefined();
+      })
+      .expect(HttpStatus.CREATED);
+  });
+
+  it('should reject duplicate registration', () => {
+    const user: RegisterDTO = {
+      username: 'username',
+      password: 'password',
+    };
+
+    return request(app)
+      .post('/auth/register')
+      .set('Accept', 'application/json')
+      .send(user)
+      .expect(({ body }) => {
+        expect(body.message).toEqual('User already exists');
+        expect(body.code).toEqual(HttpStatus.BAD_REQUEST);
+      })
+      .expect(HttpStatus.BAD_REQUEST);
+  });
+
+  it('should log user in', () => {
+    const user: LoginDTO = {
+      username: 'username',
+      password: 'password',
+    };
+
+    return request(app)
+      .post('/auth/login')
+      .set('Accept', 'application/json')
+      .send(user)
+      .expect(({ body }) => {
+        expect(body.token).toBeDefined();
+        expect(body.user.username).toEqual('username');
+        expect(body.user.password).toBeUndefined();
+      })
+      .expect(HttpStatus.CREATED);
   });
 });
